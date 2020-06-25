960eebae79e959b9f80fdf7446b9f4ce00fc739e
Micha Hulsbosch <michahulsbosch@gmail.com>
2020-06-25 13:12:06 +0200

    Added tasks app to project.

--- timetracker/settings.py
+++ timetracker/settings.py
@@ -32,12 +32,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+
+    # Apps in this project
+    'tasks',
 ]
 
 MIDDLEWARE = [
