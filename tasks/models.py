abff6d70792c816a06bc0305a2a7b8556d00be4c
Micha Hulsbosch <michahulsbosch@gmail.com>
2020-06-25 12:57:19 +0200

    Add first draft of the models.

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(length=128)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ['name', 'user']
+
+
+class Task(models.Model):
+    name = models.CharField(length=128)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Note(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Session(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField(default=None, null=True, blank=True)
