ffaf54f762820a44abdf7a294c0858b8e1a44684
Micha Hulsbosch <michahulsbosch@gmail.com>
2020-06-25 12:56:52 +0200

    Make settings point to the correct database location.

--- timetracker/settings.py
+++ timetracker/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+WRITABLE_FOLDER = '../writable/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'timetracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(WRITABLE_FOLDER, 'database', 'timtracker.db'),
     }
 }
 
