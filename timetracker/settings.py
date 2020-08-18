5a74ccec48becaabb31122b58b7e27968c69346d
Micha Hulsbosch <michahulsbosch@gmail.com>
2020-08-18 12:51:41 +0200

    Add a log out

--- timetracker/settings.py
+++ timetracker/settings.py
@@ -138,6 +138,8 @@ USE_L10N = True
 USE_TZ = True
 TIME_ZONE = 'Europe/Amsterdam'
 
+LOGOUT_REDIRECT_URL='index'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
